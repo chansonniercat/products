496843bc41fb4d18acdd6fc157d0347f74498506
Cathy M <chansonniercat@gmail.com>
2021-02-06 20:53:08 +0800

    add products

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+    name = input('Please enter name of product: ')
+    if name == 'q':
+    	break
+    price = input('Please enter price of product: ')
+    p = []
+    p.append(name) # easier: p = [name, price]
+    p.append(price)
+    products.append(p) # easier: products.append([name, price]) then no need above 3 rows
+print(products)
+
+products[0][0]
