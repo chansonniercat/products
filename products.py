6cef1f1174bbdb22b65b1fb265d0dc9a6fcdf383
Cathy M <chansonniercat@gmail.com>
2021-02-06 21:37:45 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
     if name == 'q':
     	break
     price = input('Please enter price of product: ')
+    price = int(price)
     p = []
     p.append(name) # easier: p = [name, price]
     p.append(price)
@@ -14,3 +15,11 @@ products[0][0]
 
 for p in products:
 	print('The price of', p[0], 'is', p[1])
+
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # if one of it is integer, then cannot combine, need to change back to string
+
+
