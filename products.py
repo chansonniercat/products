38ac10129a944c200dcf8cbdd34875d8eb415a41
Cathy M <chansonniercat@gmail.com>
2021-02-07 23:16:35 +0800

    add check file

--- products.py
+++ products.py
@@ -1,24 +1,35 @@
+# products [[name],[price]]
+# check file
 products = []
+import os
+if os.path.isfile('products.csv'):
+    print('Yes')
+    with open('products.csv','r', encoding='utf-8') as f:
+        for line in f:
+            if 'Chinese,Chinesee' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('No')
+
+# user to input
 while True:
     name = input('Please enter name of product: ')
     if name == 'q':
     	break
     price = input('Please enter price of product: ')
     price = int(price)
-    p = []
-    p.append(name) # easier: p = [name, price]
-    p.append(price)
-    products.append(p) # easier: products.append([name, price]) then no need above 3 rows
+    products.append([name, price])
 print(products)
-
-products[0][0]
 
 for p in products:
 	print('The price of', p[0], 'is', p[1])
 
-
+# write file in csv
 with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品, 價格\n')
+	f.write('Chinese,Chinesee\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n') # if one of it is integer, then cannot combine, need to change back to string
 
